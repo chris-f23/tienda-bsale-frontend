f309da5f88f0dcd27031845168fa2c74777ff01f
Christian Farias Aguila <christian.farias.aguila@gmail.com>
2021-08-22 18:47:09 -0400

    Implementa filtrado de productos por categoría
    - Implementa la carga de categorías desde el backend y su despliegue en el dropdown de categorías
    - Implementa filtro por categoría al seleccionar una en el dropdown de categorías
    - Limpia y documenta el código

--- public/src/api.js
+++ public/src/api.js
@@ -1,22 +1,49 @@
-const urlDesarrollo = "http://localhost:5000/api/products"
-const urlDespliegue = "https://chris-f23-bsale-backend.herokuapp.com/api/products"
+const URL_DESARROLLO = "http://localhost:5000/api"
+const URL_DESPLIEGUE = "https://chris-f23-bsale-backend.herokuapp.com/api"
 
+const urlBase = URL_DESPLIEGUE;
+
+/**
+ * Obtiene los productos desde el backend y retorna el objeto que contiene los productos y la cantidad de estos.
+ * @param {Object} parametros - Los parametros a utilizar en la petición.
+ * @param {number} parametros.cantidad - La cantidad de prodctos a obtener.
+ * @param {number} parametros.pagina - El numero de la pagina a obtener.
+ * @param {string} parametros.busqueda - El texto a buscar.
+ * @param {string} parametros.categoria - La categoria de los productos a obtener.
+ * @returns {Object} - La respuesta que contiene los productos y la cantidad de estos.
+ */
 async function obtenerProductos({
   cantidad,
   pagina,
-  busqueda
+  busqueda,
+  categoria
 }) {
-  let url = new URL(urlDespliegue);
+  let url = new URL(`${urlBase}/products`);
   let params = {};
 
   // Agregar parametros
   if (cantidad) params.limit = cantidad;
   if (pagina) params.page = pagina;
   if (busqueda) params.search = busqueda;
-
+  if (categoria) params.category = categoria;
   url.search = new URLSearchParams(params).toString();
+  
   url = url.toString();
-  console.log(`Realizando solicitud a '${url}' ...`);
+  console.log("Obteniendo productos...\n", url);
+  
+  const res = await fetch(url);
+  return res.json();
+}
+
+/**
+ * Obtiene las categorias desde el backend y retorna el objeto que contiene las categorias y la cantidad de estas.
+ * @returns {Object} - La respuesta que contiene las categorias y la cantidad de estas.
+ */
+async function obtenerCategorias() {
+  let url = new URL(`${urlBase}/categories`);
+  url = url.toString();
+
+  console.log("Obteniendo categorias...\n", url);
 
   const res = await fetch(url);
   return res.json();
--- public/src/index.js
+++ public/src/index.js
@@ -1,15 +1,21 @@
-// Plantillas.
+// #region Constantes y Variables
+
+// Plantillas para card de producto y link de paginacion.
 const PLANTILLA_PRODUCTO = document.getElementById('product_template');
+const PLANTILLA_LINK_CATEGORIA = document.getElementById('category_link_template');
 const PLANTILLA_LINK_PAGINACION = document.getElementById('page_link_template');
 
+// Elementos de la pagina.
 let contenedorProductos = document.getElementById('products_container');
 let inputBusqueda = document.getElementById('input_search');
 let contenedorLinkPaginas = document.getElementById('pagination');
 let selectCantProductos = document.getElementById('select_cant_products');
+let dropdownCategorias = document.getElementById('dropdown_categories');
 
 // Estado de la app.
 let App = {
   search: undefined,
+  category: undefined,
   pagination: {
     results_per_page: 6,
     total_results: undefined,
@@ -17,97 +23,92 @@ let App = {
   }
 }
 
-// Al cargar la pagina:
-// Obtener X productos desde el backend y crear un card por cada producto.
+// #endregion
+
+// #region Listeners
+
+/**
+ * Al cargar la pagina:
+ * - Obtiene todas las categorias y cargarlas en el dropdown de categorias.
+ * - Usando el estado inicial, obtiene los productos desde el backend y crear un card por cada producto.
+ */
 document.addEventListener("DOMContentLoaded", async (e) => {
-  const productos = await obtenerProductos({ cantidad: App.pagination.results_per_page });
-  renderizarProductos(productos.rows);
-  actualizarPaginacion(productos.count);
+  const categorias = await obtenerCategorias();
+  let linkCategoriaTodos = PLANTILLA_LINK_CATEGORIA.cloneNode(true).content;
+  linkCategoriaTodos.querySelector("a").innerHTML = "Todos";
+  linkCategoriaTodos.querySelector("a").setAttribute('onclick', `actualizarEstado({})`)
+  dropdownCategorias.appendChild(linkCategoriaTodos);
+
+  for (const categoria of categorias.rows) {
+    let linkCategoria = PLANTILLA_LINK_CATEGORIA.cloneNode(true).content;
+    linkCategoria.querySelector("a").innerHTML = categoria.name;
+    linkCategoria.querySelector("a").setAttribute('onclick', `actualizarEstado({ categoria: '${categoria.name}' })`)
+    dropdownCategorias.appendChild(linkCategoria);
+  }
+
+  await actualizarEstado();
 });
 
-// Realizar la busqueda:
+/**
+ * Al presionar el boton 'buscar' o enter dentro del input de busqueda,
+ * actualiza el valor de busqueda en el estado de la aplicacion.
+ */
 document.getElementById("search_form").addEventListener('submit', async (e) => {
   e.preventDefault();
   if (inputBusqueda.value && inputBusqueda.value.length > 0) {
-    App.search = inputBusqueda.value;
-    await buscarProductos();
+    actualizarEstado({ busqueda: inputBusqueda.value });
+  }
+
+});
+
+/**
+ * Al seleccionar una cantidad,
+ * actualiza el valor de productos por pagina en el estado de la aplicacion.
+ */
+selectCantProductos.addEventListener('change', async (e) => {
+  if (selectCantProductos.value) {
+    await actualizarEstado({ resultadosPorPagina: selectCantProductos.value});
   }
 });
 
-async function buscarProductos(pagina) {
-  if (pagina) {
-    App.pagination.current_page = pagina;
-  } else {
-    App.pagination.current_page = 1;
-  }
+// #endregion
+
+// #region Metodos
+
+/**
+ * Actualiza los atributos del estado de la aplicación.
+ * @param {Object} atributos - Los nuevos valores de los atributos del estado.
+ * @param {number} atributos.paginaActual - La nueva pagina actual.
+ * @param {string} atributos.categoria - La nueva categoria.
+ * @param {number} atributos.resultadosPorPagina - La nueva cantidad de resultados por pagina.
+ * @param {string} atributos.busqueda - La nueva busqueda.
+ */
+async function actualizarEstado({
+  paginaActual,
+  categoria,
+  resultadosPorPagina,
+  busqueda
+}) {
+  App.pagination.current_page = (paginaActual && paginaActual != null) ? paginaActual : 1;
+  App.category = (categoria && categoria != null) ? categoria : undefined;
+  App.pagination.results_per_page = (resultadosPorPagina && resultadosPorPagina != null) ? resultadosPorPagina : 6;
+  App.search = busqueda;
   
   const productos = await obtenerProductos({
     cantidad: App.pagination.results_per_page,
     pagina: App.pagination.current_page,
-    busqueda: App.search
+    busqueda: App.search,
+    categoria: App.category
   });
 
   renderizarProductos(productos.rows);
   actualizarPaginacion(productos.count);
 }
 
-// Cambiar la cantidad de productos por pagina.
-// Al seleccionar una cantidad, volver a la primera pagina para evitar quedar en una pagina vacia.
-selectCantProductos.addEventListener('change', async (e) => {
-  if (selectCantProductos.value) {
-    App.pagination.results_per_page = selectCantProductos.value;
-    await buscarProductos()
-  }
-});
-
-function actualizarPaginacion(cantidadProductos) {
-  App.pagination.total_results = cantidadProductos;
-
-  // Eliminar links antiguos.
-  while (contenedorLinkPaginas.firstChild) contenedorLinkPaginas.removeChild(contenedorLinkPaginas.firstChild);
-
-  // Crear links nuevos.
-  let cantPaginas = Math.ceil(App.pagination.total_results/App.pagination.results_per_page);
-
-  // Mostrar siempre 5 links.
-  // los numeros de las paginas cambian dependiendo de la pagina actual.
-  let offsetInicio = 0;
-  if (App.pagination.current_page > 3) {
-    offsetInicio = App.pagination.current_page - 3;
-
-    // Atajo a la primera pagina. Solo se crea si el link a la primera pagina no es visible.
-    let linkPagina = PLANTILLA_LINK_PAGINACION.cloneNode(true).content;
-    linkPagina.querySelector("a").innerHTML = `&laquo ${1}`;
-    linkPagina.querySelector("a").setAttribute('onclick', `buscarProductos(1)`)
-    contenedorLinkPaginas.appendChild(linkPagina);
-  }
-  if (App.pagination.current_page > cantPaginas - 2) {
-    offsetInicio = cantPaginas - 5;
-  }
-
-  // Links de las paginas intermedias.
-  for (let index = 1; index <= 5; index++) {
-    let linkPagina = PLANTILLA_LINK_PAGINACION.cloneNode(true).content;
-    linkPagina.querySelector("a").innerHTML = index + offsetInicio;
-    linkPagina.querySelector("a").setAttribute('onclick', `buscarProductos(${index + offsetInicio})`)
-
-    // Si es la pagina actual, agregar clase 'active'.
-    if (App.pagination.current_page === index + offsetInicio) {
-      linkPagina.querySelector("li").className += " active";
-    }
-
-    contenedorLinkPaginas.appendChild(linkPagina);
-  }
-
-  // Atajo a la ultima pagina. Solo se crea si el link a la ultima pagina no es visible.
-  if (App.pagination.current_page < cantPaginas - 2) {
-    let linkPagina = PLANTILLA_LINK_PAGINACION.cloneNode(true).content;
-    linkPagina.querySelector("a").innerHTML = `${cantPaginas} &raquo`;
-    linkPagina.querySelector("a").setAttribute('onclick', `buscarProductos(${cantPaginas})`)
-    contenedorLinkPaginas.appendChild(linkPagina);
-  }
-}
-
+/**
+ * Renderiza un card por cada producto obtenido.
+ * @param {array} productos - La lista de productos.
+ */
 function renderizarProductos(productos) {
   // Eliminar cards antiguos.
   while (contenedorProductos.firstChild) contenedorProductos.removeChild(contenedorProductos.firstChild);
@@ -131,3 +132,59 @@ function renderizarProductos(productos) {
     contenedorProductos.appendChild(cardProducto);
   }
 }
+
+/**
+ * Actualiza los links de paginación.
+ * Utiliza el estado de la aplicación y la cantidad total de productos
+ * para determinar la forma en la que se muestran los links.
+ * @param {number} cantidadTotalProductos - La cantidad total de productos.
+ */
+function actualizarPaginacion(cantidadTotalProductos) {
+  App.pagination.total_results = cantidadTotalProductos;
+
+  // Eliminar links antiguos.
+  while (contenedorLinkPaginas.firstChild) contenedorLinkPaginas.removeChild(contenedorLinkPaginas.firstChild);
+
+  // Crear links nuevos.
+  let cantPaginas = Math.ceil(App.pagination.total_results/App.pagination.results_per_page);
+
+  // Mostrar siempre 5 links.
+  // los numeros de las paginas cambian dependiendo de la pagina actual.
+  let offsetInicio = 0;
+  if (App.pagination.current_page > 3) {
+    offsetInicio = App.pagination.current_page - 3;
+
+    // Atajo a la primera pagina. Solo se crea si el link a la primera pagina no es visible.
+    let linkPagina = PLANTILLA_LINK_PAGINACION.cloneNode(true).content;
+    linkPagina.querySelector("a").innerHTML = `&laquo ${1}`;
+    linkPagina.querySelector("a").setAttribute('onclick', `actualizarEstado({ pagina: 1 })`)
+    contenedorLinkPaginas.appendChild(linkPagina);
+  }
+  if (App.pagination.current_page > cantPaginas - 2) {
+    offsetInicio = cantPaginas - 5;
+  }
+
+  // Links de las paginas intermedias.
+  for (let index = 1; index <= 5; index++) {
+    let linkPagina = PLANTILLA_LINK_PAGINACION.cloneNode(true).content;
+    linkPagina.querySelector("a").innerHTML = index + offsetInicio;
+    linkPagina.querySelector("a").setAttribute('onclick', `actualizarEstado({ pagina: ${index + offsetInicio} })`)
+
+    // Si es la pagina actual, agregar clase 'active'.
+    if (App.pagination.current_page === index + offsetInicio) {
+      linkPagina.querySelector("li").className += " active";
+    }
+
+    contenedorLinkPaginas.appendChild(linkPagina);
+  }
+
+  // Atajo a la ultima pagina. Solo se crea si el link a la ultima pagina no es visible.
+  if (App.pagination.current_page < cantPaginas - 2) {
+    let linkPagina = PLANTILLA_LINK_PAGINACION.cloneNode(true).content;
+    linkPagina.querySelector("a").innerHTML = `${cantPaginas} &raquo`;
+    linkPagina.querySelector("a").setAttribute('onclick', `actualizarEstado({ pagina: ${cantPaginas})`)
+    contenedorLinkPaginas.appendChild(linkPagina);
+  }
+}
+
+// #endregion
